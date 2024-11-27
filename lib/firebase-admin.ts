964f272527c84ebf398a278e54f37757592b947a
technocloudsolutions <technocloudsolution@gmail.com>
2024-11-27 16:08:53 +0530

    new3

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,3 +1,18 @@
+import * as admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const db = getFirestore(); 
+if (!admin.apps.length) {
+  // Parse the service account JSON from environment variable
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}'
+  );
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    // projectId: process.env.FIREBASE_PROJECT_ID
+  });
+}
+
+const db = getFirestore();
+
+export { db }; 
