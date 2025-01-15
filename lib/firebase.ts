c787ab9a2a40270068b04f7b03e1a1b3d61183e4
technocloudsolutions <technocloudsolution@gmail.com>
2025-01-15 23:09:48 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -14,5 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, 'yssc-dev');
+export const db = getFirestore(app);
 export const storage = getStorage(app);
